4836c1ad414f042382575758b3ea5540783b663a
Yuuzi261 <lokasu0531@gmail.com>
2024-09-19 20:50:18 +0800

    feat: add numeric input

--- src/components/SelectRows.vue
+++ src/components/SelectRows.vue
@@ -1,17 +1,23 @@
 <template>
 <div class="slider-container">
-	<h2>{{ title }}</h2>
-	<vue-slider 
-		v-model="sliderStore.rows"
-		:min="0"
-		:max="50"
-		:interval="1"
-		:contained="true"
-		:lazy="true"
-		:drag-on-click="true"
-		:marks="[0, 50]"
-		@change="sliderStore.setValue">
-	</vue-slider>
+  <div class="top-container">
+      <h2>{{ title }}</h2>
+    <div class="input-container">
+      <input v-model="sliderStore.rows" disabled="disabled" />
+      <button @click="decreaseValue">-</button>
+      <button @click="increaseValue">+</button>
+    </div>
+  </div>
+  <vue-slider 
+    v-model="sliderStore.rows"
+    :min="0"
+    :max="50"
+    :interval="1"
+    :contained="true"
+    :lazy="true"
+    :drag-on-click="true"
+    :marks="[0, 50]">
+  </vue-slider>
 </div>
 </template>
 
@@ -22,18 +28,79 @@ import '/styles/themes/slider.css'
 
 const title = 'Number Of Rows';
 const sliderStore = useSliderStore();
+
+const increaseValue = () => {
+  if (sliderStore.rows < 50) {
+    sliderStore.rows++;
+  }
+};
+
+const decreaseValue = () => {
+  if (sliderStore.rows > 0) {
+    sliderStore.rows--;
+  }
+};
 </script>
 
 <style scoped>
+.top-container {
+  margin-bottom: 1.5vh;
+  display: flex;
+}
+
+.input-container {
+  display: flex;
+  align-items: center;
+  margin-left: 2vh;
+  border: 1px solid rgba(0, 0, 0, 0);
+  border-radius: 5px;
+  overflow: hidden;
+  transition: 0.3s ease;
+}
+
+.input-container:hover {
+  border: 1px solid #f44949;
+  transition: 0.3s ease;
+}
+
+.input-container:hover input,
+.input-container:hover button {
+  border: none;
+}
+
 .slider-container {
-    width: 100%;
-    padding: 20px;
-    background-color: transparent;
-    box-sizing: border-box;
+  width: 100%;
+  padding: 20px;
+  background-color: transparent;
+  box-sizing: border-box;
 }
 
 h2 {
-    margin: 0 0 10px 0;
-    color: #ffffff;
+  margin-bottom: 1vh;
+  color: #ffffff;
+}
+
+input {
+  background-color: #262730;
+  color: #ffffff;
+  text-align: center;
+  height: 100%;
+  width: 8vh;
+  margin: none;
+  border: none;
+}
+
+button {
+  background-color: #262730;
+  height: 100%;
+  width: 3.5vh;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #f44949;
 }
 </style>
