7860a2fc691b0c1fffc5bf3ebd02517ca9adc485
Yuuzi261 <lokasu0531@gmail.com>
2024-09-24 02:11:04 +0800

    style: change icon of delete row button

--- src/components/SelectSkill.vue
+++ src/components/SelectSkill.vue
@@ -73,7 +73,7 @@ const deleteRow = (index) => {
 <template>
   <div v-for="i in sliderStore.rows" :key="i" class="container">
     <button class="fixed-button" @click="deleteRow(i-1)">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/></svg>
     </button>
     <div class="column">
       <div class="empty"></div>
