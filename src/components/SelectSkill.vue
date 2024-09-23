40a037c9669cba93020a3350f719154293cf1d07
Yuuzi261 <lokasu0531@gmail.com>
2024-09-24 02:05:30 +0800

    feat: add delete row button

--- src/components/SelectSkill.vue
+++ src/components/SelectSkill.vue
@@ -62,10 +62,19 @@ const targetOptions = computed(() => {
     .filter(selection => selection.character !== null && selection.style !== null)
     .map(selection => selection.character);
 });
+
+const deleteRow = (index) => {
+  sliderStore.rows -= 1;
+  skillStore.turns.splice(index, 1);
+  skillStore.skills.splice(index, 1);
+};
 </script>
 
 <template>
   <div v-for="i in sliderStore.rows" :key="i" class="container">
+    <button class="fixed-button" @click="deleteRow(i-1)">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+    </button>
     <div class="column">
       <div class="empty"></div>
       <Multiselect
@@ -136,6 +145,7 @@ span {
 }
 .container {
   display: grid; 
+  position: relative;
   gap: 10px;
   padding: 20px;
   border-radius: 20px;
@@ -177,6 +187,23 @@ span {
 }
 .selected-button:hover .char-img {
   transform: scale(1.1);
+}
+.fixed-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  cursor: pointer;
+  border: none;
+}
+.fixed-button svg {
+  width: 24px;
+  height: 24px;
+  fill: #ffffff;
+  transition: fill 0.3s ease;
+}
+.fixed-button:hover svg {
+  fill: #EA3323;
 }
 .char-img{
   transition: transform 0.3s ease;
