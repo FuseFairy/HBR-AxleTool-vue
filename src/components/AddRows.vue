1c6d9b9df1886507e72ed2b87cfff4ad663e3f0c
Yuuzi261 <lokasu0531@gmail.com>
2024-09-24 11:32:33 +0800

    style: change add row button style

--- src/components/AddRows.vue
+++ src/components/AddRows.vue
@@ -4,7 +4,7 @@
 		@click="addRow"
 		class="circle-button add-button"
 		:disabled="sliderStore.rows === 50 || (sliderStore.rows === 0 && !hasChar)">
-		<img class="icon-img" src="@/assets/custom_icon/add.svg" alt="Add a row" />
+		<img class="icon-img" src="@/assets/custom_icon/add_circle.svg" alt="Add a row" />
 	</button>
 </div>
 </template>
@@ -53,42 +53,34 @@ const scrollToBottom = () => {
   align-items: center;
   height: auto;
 }
-
 .circle-button {
   margin-top: 20px;
-  border-radius: 50%;
   width: 80px;
   height: 80px;
-  aspect-ratio: 1 / 1;
   font-size: 16px;
-  overflow: hidden;
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: rgba(79, 74, 74, 0.5);
+  background-color: transparent;
 }
-
 .add-button {
   border: none;
-  transition: background-color 0.3s ease;
-}
-.add-button:hover {
-  background-color: rgba(204, 201, 201, 0.5);
 }
 .add-button:hover .icon-img {
-	filter: invert(1);
+	filter: brightness(1.5);
+  transform: scale(1.1);
+  transition: 0.3s ease;
 }
-
 .icon-img {
-  width: 50px;
-  height: 50px;
+  transition: 0.3s ease;
+  transform-origin: center;
+  width: 80px;
+  height: 80px;
 }
-
 button:disabled {
   display: none;
 }
-
 @media (max-width: 800px) {
   .container {
     width: auto;
