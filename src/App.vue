1b53ebddfaad41cedcd7fca3104971d9a0d1b1f7
FuseFairy <dd8611706@gmail.com>
2024-12-03 22:26:32 +0800

    refactor: can't copy bg

--- src/App.vue
+++ src/App.vue
@@ -73,9 +73,23 @@ const toggleTable = () => {
     'navbar'
     'main';
   background-color: black;
+  position: relative;
+  z-index: 0;
+  overflow: hidden;
+}
+.page-layout::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background-image: url(@/assets/bg.webp);
   background-position: center;
   background-size: cover;
+  background-repeat: no-repeat;
+  z-index: -1;
+  pointer-events: none;
 }
 nav {
   display: flex;
