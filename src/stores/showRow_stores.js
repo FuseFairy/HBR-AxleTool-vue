817161987e0af2afe531d31520d91a28b44f70b6
FuseFairy <dd8611706@gmail.com>
2024-12-12 19:13:25 +0800

    feat: show multiple teams

--- src/stores/showRow_stores.js
+++ src/stores/showRow_stores.js
@@ -4,7 +4,7 @@ import { ref, computed } from 'vue'
 export const useShowRowStore = defineStore(
   'showRow',
   () => {
-    const showRow = ref(['rank', 'earring', 'passive skill', 'skill'])
+    const showRow = ref(['rank', 'earring', 'passive skill', 'skill', 'axle'])
 
     function setShowRow(row_list) {
       showRow.value = row_list
