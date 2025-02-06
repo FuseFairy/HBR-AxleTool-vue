4b849b6a9b52fd4fafcd7948d23864eac01a17d7
FuseFairy <dd8611706@gmail.com>
2025-02-06 14:52:56 +0800

    refactor: optimize domtoimage structure

--- src/scripts/domToImage.js
+++ src/scripts/domToImage.js
@@ -8,66 +8,64 @@ import domtoimage from 'dom-to-image-more'
 
 export async function convertElementToJpg(elementId) {
   const element = document.getElementById(elementId)
+  if (!element) {
+    console.error(`Element with ID "${elementId}" not found.`)
+    return
+  }
+
   const charStore = useCharStore()
   const skillStore = useSkillStore()
   const sliderStore = useSliderStore()
   const usedTeams = getUsedTeams()
+
   const usedCharStore = usedTeams.reduce((result, team) => {
-    if (team in charStore.selections) {
-      result[team] = charStore.selections[team]
+    const characters = charStore.selections[team]
+    if (characters) {
+      result[team] = Object.fromEntries(
+        Object.entries(characters).map(([charName, charData]) => [
+          charName,
+          { ...charData, skill: [] }
+        ])
+      )
     }
     return result
   }, {})
+
   const axleName = skillStore.axleName.trimEnd()
 
-  if (element) {
-    try {
-      const newCharStore = Object.fromEntries(
-        Object.entries(usedCharStore).map(([team, characters]) => [
-          team,
-          Object.fromEntries(
-            Object.entries(characters).map(([charName, charData]) => [
-              charName,
-              { ...charData, skill: [] }
-            ])
-          )
-        ])
-      );      
+  try {
+    const dataUrl = await domtoimage.toJpeg(element, {
+      quality: 1.0,
+      backgroundColor: 'black',
+      cacheBust: true,
+      width: element.scrollWidth,
+      height: element.scrollHeight,
+    })
 
-      const dataUrl = await domtoimage.toJpeg(element, {
-        quality: 1.0,
-        backgroundColor: 'black',
-        cacheBust: true,
-        width: element.scrollWidth,
-        height: element.scrollHeight,
-      });
-
-      const customData = {
-        char: newCharStore,
-        axleName: axleName,
-        skills: skillStore.skills,
-        turns: skillStore.turns,
-        rows: sliderStore.rows
-      };
-
-      const jsonString = JSON.stringify(customData);
-      const compressedData = compressToBase64(jsonString);
-
-      const exif = {};
-      exif[piexif.ExifIFD.UserComment] = compressedData;
-
-      const exifObj = { Exif: exif };
-      const exifBytes = piexif.dump(exifObj);
-      const jpegWithExifData = piexif.insert(exifBytes, dataUrl);
-
-      const link = document.createElement('a');
-      link.href = jpegWithExifData;
-      link.download = `${axleName || 'hbr_axle'}.jpg`;
-      link.click();
-    } catch (error) {
-      console.error('Error generating or saving image:', error)
+    const customData = {
+      char: usedCharStore,
+      axleName: axleName,
+      skills: skillStore.skills,
+      turns: skillStore.turns,
+      rows: sliderStore.rows
     }
-  } else {
-    console.error(`Element with ID "${elementId}" not found.`)
+
+    const jsonString = JSON.stringify(customData)
+    const compressedData = compressToBase64(jsonString)
+    const exif = {
+      [piexif.ExifIFD.UserComment]: compressedData,
+    }
+
+    const exifObj = { Exif: exif }
+    const exifBytes = piexif.dump(exifObj)
+    const jpegWithExifData = piexif.insert(exifBytes, dataUrl)
+
+    const link = document.createElement('a')
+    link.href = jpegWithExifData
+    link.download = `${axleName || 'hbr_axle'}.jpg`
+    link.click()
+
+  } catch (error) {
+    console.error('Error generating or saving image:', error)
   }
 }
