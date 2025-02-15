6871de49cb6a2090c97066ae19eed040bd80f58a
FuseFairy <dd8611706@gmail.com>
2025-02-15 10:04:18 +0800

    refactor: update sort

--- src/scripts/getUsedSkills.js
+++ src/scripts/getUsedSkills.js
@@ -1,11 +1,13 @@
 import { useCharStore } from '@/stores/char_stores'
 import { useSkillStore } from '@/stores/skill_stores'
+import { useSettingStore } from '@/stores/setting_stores'
 import _find from 'lodash/find'
 
 export function getUsedSkills(selectedTab) {
   const skillsDictionary = {}
   const charStore = useCharStore()
   const skillStore = useSkillStore()
+  const settingStore = useSettingStore()
 
   Object.values(charStore.selections[selectedTab]).forEach((selection) => {
     const style = selection['style']
@@ -25,14 +27,26 @@ export function getUsedSkills(selectedTab) {
         }
       })
     })
+
+    const sortedSkills = (() => {
+      const skillNameMap = new Map();
+      selection['skill'].forEach(skill => {
+        skillNameMap.set(skill.value, skill["names"][settingStore.lang]);
+      });
+      
+      const skillsArray = Array.from(skillsDictionary[style]);
+    
+      skillsArray.sort((a, b) => {
+        const nameA = skillNameMap.get(a); 
+        const nameB = skillNameMap.get(b);
+        return nameA.length - nameB.length;
+      });
+      return skillsArray;
+    })();
+    
+    skillsDictionary[style] = new Set(sortedSkills);
   })
 
-  Object.keys(skillsDictionary).forEach((style) => {
-    const sortedSkills = Array.from(skillsDictionary[style])
-      .sort((a, b) => a.length - b.length)
-    skillsDictionary[style] = new Set(sortedSkills)
-  })
-  
   return skillsDictionary
 }
 
