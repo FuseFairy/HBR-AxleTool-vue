ac6ece6afabc2a4c82f6eb1f3aa4a5cbb6b83be5
FuseFairy <dd8611706@gmail.com>
2025-02-15 11:07:03 +0800

    fix: style null error

--- src/scripts/getUsedSkills.js
+++ src/scripts/getUsedSkills.js
@@ -14,37 +14,37 @@ export function getUsedSkills(selectedTab) {
 
     if (style && !skillsDictionary[style]) {
       skillsDictionary[style] = new Set()
-    }
 
-    skillStore.skills.forEach((group) => {
-      group.forEach((skillEntry) => {
-        if (skillEntry.style === style && skillEntry.selectedTab === selectedTab) {
-          const { skill } = skillEntry;
-          const foundCommandSkill = _find(selection['commandSkill'], { value: skill });
-          if (skill && !skillsDictionary[style].has(skill) && !foundCommandSkill) {
-            skillsDictionary[style].add(skill);
+      skillStore.skills.forEach((group) => {
+        group.forEach((skillEntry) => {
+          if (skillEntry.style === style && skillEntry.selectedTab === selectedTab) {
+            const { skill } = skillEntry;
+            const foundCommandSkill = _find(selection['commandSkill'], { value: skill });
+            if (skill && !skillsDictionary[style].has(skill) && !foundCommandSkill) {
+              skillsDictionary[style].add(skill);
+            }
           }
-        }
+        })
       })
-    })
-
-    const sortedSkills = (() => {
-      const skillNameMap = new Map();
-      selection['skill'].forEach(skill => {
-        skillNameMap.set(skill.value, skill["names"][settingStore.lang]);
-      });
+  
+      const sortedSkills = (() => {
+        const skillNameMap = new Map();
+        selection['skill'].forEach(skill => {
+          skillNameMap.set(skill.value, skill["names"][settingStore.lang]);
+        });
+        
+        const skillsArray = Array.from(skillsDictionary[style]);
       
-      const skillsArray = Array.from(skillsDictionary[style]);
-    
-      skillsArray.sort((a, b) => {
-        const nameA = skillNameMap.get(a); 
-        const nameB = skillNameMap.get(b);
-        return nameA.length - nameB.length;
-      });
-      return skillsArray;
-    })();
-    
-    skillsDictionary[style] = new Set(sortedSkills);
+        skillsArray.sort((a, b) => {
+          const nameA = skillNameMap.get(a); 
+          const nameB = skillNameMap.get(b);
+          return nameA.length - nameB.length;
+        });
+        return skillsArray;
+      })();
+      
+      skillsDictionary[style] = new Set(sortedSkills);
+    }
   })
 
   return skillsDictionary
