e9f72cf1429acbc354fb194f8cbe44dcb3b8352e
FuseFairy <dd8611706@gmail.com>
2025-04-05 10:05:20 +0800

    feat: add rollup-plugin-visualizer for bundle analysis

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+    }),
   ],
   resolve: {
     alias: {
