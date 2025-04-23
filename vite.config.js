a0a75d14b6fbe86e19245e3e35a7677ee2299737
FuseFairy <dd8611706@gmail.com>
2025-04-24 05:13:13 +0800

    feat: add terser to minify js file

--- vite.config.js
+++ vite.config.js
@@ -34,5 +34,12 @@ export default defineConfig({
         })
       ],
     },
+    minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+    }
   }
 })
