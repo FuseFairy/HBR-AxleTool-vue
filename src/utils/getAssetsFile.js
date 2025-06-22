5cfc3af6dfbbf3b89c02fed121e960b159468807
FuseFairy <dd8611706@gmail.com>
2025-06-22 12:29:51 +0800

    refactor: use import.meta.glob to get assets file

--- src/utils/getAssetsFile.js
+++ src/utils/getAssetsFile.js
@@ -1,25 +1,10 @@
+const assetModules = import.meta.glob('/src/assets/**/*', { 
+  eager: true, 
+  query: '?url',
+  import: 'default'
+});
+
 export function getAssetsFile(path) {
-  const pathArr = path.split('/')
-  switch (pathArr.length) {
-    case 1:
-      return new URL(`/src/assets/${path}`, import.meta.url).href
-    case 2:
-      return new URL(`/src/assets/${pathArr[0]}/${pathArr[1]}`, import.meta.url).href
-    case 3:
-      return new URL(`/src/assets/${pathArr[0]}/${pathArr[1]}/${pathArr[2]}`, import.meta.url).href
-    case 4:
-      return new URL(`/src/assets/${pathArr[0]}/${pathArr[1]}/${pathArr[2]}/${pathArr[3]}`, import.meta.url).href
-    case 5:
-      return new URL(
-        `/src/assets/${pathArr[0]}/${pathArr[1]}/${pathArr[2]}/${pathArr[3]}/${pathArr[4]}`,
-        import.meta.url
-      ).href
-    case 6:
-      return new URL(
-        `/src/assets/${pathArr[0]}/${pathArr[1]}/${pathArr[2]}/${pathArr[3]}/${pathArr[4]}/${pathArr[5]}`,
-        import.meta.url
-      ).href
-    default:
-      throw new Error('Invalid path')
-  }
+  const fullPathInSrc = `/src/assets/${path}`;
+  return assetModules[fullPathInSrc];
 }
