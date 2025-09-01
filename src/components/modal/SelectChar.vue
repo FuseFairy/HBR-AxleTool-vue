fcfdebeff5bd34c9a2805cf48107888f09d258cc
FuseFairy <dd8611706@gmail.com>
2025-09-01 23:05:19 +0800

    style: change bg color

--- src/components/modal/SelectChar.vue
+++ src/components/modal/SelectChar.vue
@@ -474,7 +474,7 @@
     left: 0;
     width: 100%;
     height: 100vh;
-    background-color: var(--bg-color-dark-transparent-2);
+    background-color: rgba(0, 0, 0, 0.5);
     justify-content: center;
     align-items: center;
     z-index: 1100;
